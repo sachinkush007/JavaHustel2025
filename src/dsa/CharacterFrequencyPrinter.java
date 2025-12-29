ecdab49215152895ac022f900f280dd95a810099
sachinm.kumar <sachinm.kumar@aurionpro.com>
2025-12-29 12:28:52 +0530

    NEW Changes

---
+++ src/dsa/CharacterFrequencyPrinter.java
@@ -0,0 +1,21 @@
+package dsa;
+
+public class CharacterFrequencyPrinter {
+    public static void main(String[] args) {
+
+        String s="2a3b4c5d";
+
+        for(int i=0;i<s.length();i++){
+            if(Character.isDigit(s.charAt(i))){
+//                int count=s.charAt(i)-'0';
+                int count=Integer.parseInt(String.valueOf(s.charAt(i)));
+                System.out.println(count);
+                char ch=s.charAt(i+1);
+
+                for(int j=0;j<count;j++){
+                    System.out.print(j);
+                }
+            }
+        }
+    }
+}
